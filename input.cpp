4f3109d8baa4ee2f1d257e2a3882e28797429b91
Juan Miguel I. Moreno <juan.miguel.im25@gmail.com>
2023-01-12 09:36:15 -0600

    cambios en comentarios

--- input.cpp
+++ input.cpp
@@ -1,10 +1,13 @@
+// @author JMIM
+// @date 12-01-2023
+// Ejemplo para ingresar datos en la consola
 #include <iostream>
 using namespace std;
 
 int main() {
   int x;
-  cout << "Type a number: "; // Type a number and press enter
-  cin >> x; // Get user input from the keyboard
-  cout << "Your number is: " << x;
-  return 0;
+  cout << "Escribe un numero: "; // escribes y pulsa enter
+  cin >> x; // lee el input del teclado
+  cout << "El numero ingresado fue: " << x; // muestra el contenido de la variable
+  return 0; // retorna 0 para finalizar la ejecución 
 }
