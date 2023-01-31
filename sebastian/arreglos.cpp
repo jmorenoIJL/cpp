a4691514b049d4ea725b64b5267be762f5238d5c
Sebas <charrintorres@gmail.com>
2023-01-31 10:59:24 -0600

    Arreglos 2.0

--- sebastian/arreglos.cpp
+++ sebastian/arreglos.cpp
@@ -4,27 +4,29 @@ using namespace std;
 int main()
 {
     int n = 0;
-
     cout << "¿Cuantos numeros quieres ver?: " << endl;
-    cin >> n;    
-
+    cin >> n;
     int arreglo[n];
-   
-    for (int i = 0; i < n; i++) {
-        cout << "Ingrese un numero: ";
-        cin >> arreglo[i];
-    }
     
-    cout << "Los numeros que contiene el array son: " << "\n";
+
     for (int i = 0; i < n; i++) {
-        cout << arreglo[i] << "\n";
+        //  cout << "Ingrese un numero: ";
+        arreglo[i] = rand() % 100;
+
     }
 
-    cout << "Los numeros pares que contiene el arreglo son: " << "\n";
-    for (int i = 0; i < n; i++) {
-        if (arreglo[i] % 2 == 0) cout << arreglo[i] << "\n";
-    }
+    
+      cout << "Los numeros que contiene el array son: " << "\n";
+      for (int i = 0; i < n; i++) {
+          cout << arreglo[i] << "\n";
+      }
 
-    return 0;
+      cout << "Los numeros pares que contiene el arreglo son: " << "\n";
+      for (int i = 0; i < n; i++) {
+          if (arreglo[i] % 2 == 0) cout << arreglo[i] << "\n";
+      }
+
+      return 0;
+      
 
 }
